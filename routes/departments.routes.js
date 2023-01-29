a67a3246f1feb9db9392f277929df749da738f54
Andrzej Popiolek <xandrzejpopiolek@gmail.com>
2023-01-29 23:17:54 +0100

    Change message when editing and deleting departments

--- routes/departments.routes.js
+++ routes/departments.routes.js
@@ -113,7 +113,7 @@ router.put('/departments/:id', async (req, res) => {
     if (dep) {
       dep.name = name;
       await dep.save();
-      res.json({ message: dep });
+      res.json({ message: 'OK', editedDepartment: dep });
     } else res.status(404).json({ message: 'Not found...' });
   } catch (err) {
     res.status(500).json({ message: err });
@@ -136,7 +136,7 @@ router.delete('/departments/:id', async (req, res) => {
     const dep = await Department.findById(req.params.id);
     if (dep) {
       await Department.deleteOne({ _id: req.params.id });
-      res.json({ message: dep });
+      res.json({ message: 'OK', deletedDepartment: dep });
     } else res.status(404).json({ message: 'Not found...' });
   } catch (err) {
     res.status(500).json({ message: err });
